773f090d8668bcca35fbf7ea91437bd2455d439b
Code Sandbox <hello@codesandbox.io>
2021-11-17 09:13:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3694303c5aea8b56621e35a758dadba7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff70d430401e2d8fb478a78dc1720a0",
+    "url": "https://lisagh.github.io/csb-5zkr0/index.html"
+  },
+  {
+    "revision": "e0ba24d7256022b89db1",
+    "url": "https://lisagh.github.io/csb-5zkr0/static/js/2.0d8bed9b.chunk.js"
+  },
+  {
+    "revision": "1ace2e7634e6c85b3836",
+    "url": "https://lisagh.github.io/csb-5zkr0/static/js/main.7230b46d.chunk.js"
+  },
+  {
+    "revision": "6b1ef32177bfd1c94048",
+    "url": "https://lisagh.github.io/csb-5zkr0/static/js/runtime~main.b3985917.js"
+  }
+]);
